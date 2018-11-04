aaaef1a4b57f61ffb15ef4eb465214bba7e8097b
Rushabh <rspate1995@yahoo.com>
2018-11-04 14:10:22 +0530

    jupyter notebook added

--- solverExp.py
+++ solverExp.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-destinations = pd.read_csv("destinations.csv")
-test = pd.read_csv("test.csv")
-train = pd.read_csv("train.csv")
+destinations = pd.read_csv("data/destinations.csv")
+test = pd.read_csv("data/test.csv")
+train = pd.read_csv("data/train.csv")
 
